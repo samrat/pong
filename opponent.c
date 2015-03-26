77168f3d4e3f51e7720cac1e94c97fe0a7fd2799
Samrat Man Singh <samratmansingh@gmail.com>
2015-03-26 22:14:15 +0530

    CPU Opponent only moves if ball is going towards it.

--- opponent.c
+++ opponent.c
@@ -2,13 +2,15 @@
 
 static void
 opponent_move(game_input *opponent_input, player_info *opponent, ball_info *ball) {
-
+  int ball_dir = ball->dx;
   int ball_dist = ball->y - opponent->y;
 
-  if (ball_dist < 0) {
-    opponent_input->move_up = true;
-  }
-  else if (ball_dist > 0) {
-    opponent_input->move_down = true;
+  if (ball_dir > 0) {
+    if (ball_dist < 0) {
+      opponent_input->move_up = true;
+    }
+    else if (ball_dist > 0) {
+      opponent_input->move_down = true;
+    }
   }
 }
