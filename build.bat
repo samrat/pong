ef190b1616945428c1a2c228b2bdc8e13f457394
Samrat Man Singh <samratmansingh@gmail.com>
2015-04-03 23:46:17 +0530

    Use actual Windows Kits path

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
-REM C:\winkits\Include\shared is "C:\Window Kits\8.0\"
-
-gcc sdl_pong.c -std=c99 -IC:\mingw_dev_lib\include\SDL2 -IC:\winkits\Include\shared -LC:\mingw_dev_lib\lib -w -Wl,-subsystem,windows -lmingw64 -lSDL2main -lSDL2 -o pm
-gcc -g sdl_pong.c -std=c99 -IC:\mingw_dev_lib\include\SDL2 -IC:\winkits\Include\shared -LC:\mingw_dev_lib\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o pm
+del pm.exe
+gcc -g sdl_pong.c -std=c99 -IC:\mingw_dev_lib\include\SDL2 -I "C:\Program Files (x86)\Windows Kits\8.1\Include\shared" -LC:\mingw_dev_lib\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o pm
